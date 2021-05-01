db641f029c4d26ecebf014c9ac97ba0c1c2258c4
Irina Boiko <irinaboiko95@gmail.com>
2021-05-01 19:41:02 +0300

    79: redux-form stopSubmit

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,6 +5,7 @@ import {required} from "../../utils/validators/validators";
 import {connect} from "react-redux";
 import {login} from "../../redux/auth-reducer";
 import {Redirect} from "react-router-dom";
+import styles from './../common/FormsControls/FormsControls.module.css';
 
 const LoginForm = (props) => {
     return (
@@ -20,6 +21,9 @@ const LoginForm = (props) => {
             <div>
                 <Field component={"input"} type={"checkbox"} name={"rememberMe"} /> Remember me
             </div>
+            { props.error && <div className={styles.formSummaryError}>
+                {props.error}
+            </div> }
             <div>
                 <button>Log In</button>
             </div>
--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -9,21 +9,28 @@ import {compose} from "redux";
 class ProfileContainer extends React.Component {
     componentDidMount() {
         let userId = this.props.match.params.userId;
-        if (!userId) {userId = 13224;}
+        if (!userId) {
+            userId = this.props.authorizedUserId;
+        }
         this.props.getUserProfile(userId);
         this.props.getStatus(userId);
     }
 
     render() {
         return (
-            <Profile {...this.props} profile={this.props.profile} status={this.props.status} updateStatus={this.props.updateStatus} />
+            <Profile {...this.props}
+                     profile={this.props.profile}
+                     status={this.props.status}
+                     updateStatus={this.props.updateStatus} />
         )
     }
 }
 
 const mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
-    status: state.profilePage.status
+    status: state.profilePage.status,
+    authorizedUserId: state.auth.userId,
+    isAuth: state.auth.isAuth
 });
 
 export default compose(
